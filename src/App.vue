b52c26acff45d5f019d74ca358355e97d8050a0b
Leny Mory <leny.mory@divtec.ch>
2024-11-30 18:56:59 +0100

    ajouts d'image, header et footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <app-header />
     <v-main class="mr-auto ml-auto">
       <router-view />
     </v-main>
@@ -9,6 +10,7 @@
 
 <script setup>
   import AppFooter from "@/components/AppFooter.vue"
+  import AppHeader from "@/components/AppHeader.vue"
 
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+const items = [
+  { path: '/', title: 'Classement' },
+  { path: '/matchs', title: 'Matchs' },
+  { path: '/jeu', title: 'Jeux' },
+]
 </script>
 
 <template>
-
+  <v-app-bar app>
+    <v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn class="pt-2 pb-2"text v-for="item in items" :key="item.title" :to="item.path">{{ item.title }}</v-btn>
+      </v-toolbar-items>
+    </v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <style scoped lang="sass">
